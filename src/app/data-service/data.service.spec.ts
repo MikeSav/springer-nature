0867bbe1ccc3ed534fa77a9286db324e7ddb3aea
Michael Savage <msavage@W299963.local>
2017-04-25 12:42:10 +0200

    Saw issue in Hamjest, needed to reduce LoDash

--- src/app/data-service/data.service.spec.ts
+++ src/app/data-service/data.service.spec.ts
@@ -1,29 +1,35 @@
 import { async, TestBed, inject } from '@angular/core/testing';
 import {
-	ResponseOptions,
-	BaseRequestOptions,
-	Http,
-	XHRBackend
+    ResponseOptions,
+    BaseRequestOptions,
+    Http,
+    Response,
+    XHRBackend
 } from '@angular/http';
 import {
-	MockBackend,
-	MockConnection,
+    MockBackend,
+    MockConnection,
 } from '@angular/http/testing';
 
 import {
-	assertThat,
-	is,
-	truthy,
-	hasProperties,
-	contains
+    assertThat,
+    is,
+    truthy,
+    hasProperties,
+    contains
 } from 'hamjest';
 
 import {
-	wasCalled
+    SinonStub,
+    stub
+} from 'sinon'
+
+import {
+    wasCalled
 } from '../test/matcher/wasCalled';
 
 import {
-	returnXMLdoc
+    returnXMLdoc
 } from '../test/data-functions/mockXML';
 
 import { WindowService } from '../window-service/window.service';
@@ -31,41 +37,67 @@ import { WindowService } from '../window-service/window.service';
 import { DataService } from './data.service';
 
 describe('DataService', () => {
-	beforeEach(() => {
-		TestBed.configureTestingModule({
-			providers: [
-				DataService,
-				MockBackend,
-				BaseRequestOptions,
-				WindowService,
-				{
-					provide: Http,
-					deps: [MockBackend, BaseRequestOptions],
-					useFactory: (backendInstance: XHRBackend, defaultOptions: BaseRequestOptions) => {
-						return new Http(backendInstance, defaultOptions);
-					}
-				}
-			]
-		});
-	});
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                DataService,
+                MockBackend,
+                BaseRequestOptions,
+                WindowService, // ToDo: Mock this,
+                {
+                    provide: Http,
+                    deps: [MockBackend, BaseRequestOptions],
+                    useFactory: (backendInstance: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                        return new Http(backendInstance, defaultOptions);
+                    }
+                }
+            ]
+        });
+    });
 
-	let sut: DataService;
-	let backend: MockBackend;
+    let sut: DataService;
+    let backend: MockBackend;
 
-	beforeEach(inject([
-		DataService, MockBackend
-	], (dataService: DataService, mockBackend: MockBackend) => {
-		sut = dataService;
-		backend = mockBackend;
-	}));
+    beforeEach(inject([
+        DataService, MockBackend
+    ], (dataService: DataService, mockBackend: MockBackend) => {
+        sut = dataService;
+        backend = mockBackend;
+    }));
 
-	it('should create an instance', () => {
+    it('should create an instance', () => {
 
-		assertThat(sut, is(truthy()));
-	});
+        assertThat(sut, is(truthy()));
+    });
 
+    describe('getXML()', () => {
 
-	describe('getXML()', () => {
+        it('should call backend with headers', async(() => {
+            const assertionCall: SinonStub = stub();
+            backend.connections.subscribe((connection: MockConnection) => {
+                assertThat(connection.request.headers.toJSON().Accept, contains('application/xml')
+                );
+                assertionCall();
+            });
+            sut.getXML();
 
-	});
+            assertThat(assertionCall, wasCalled());
+        }));
+
+        xit('should return XML data', async(() => {
+
+            const assertionCall: SinonStub = stub();
+            backend.connections.subscribe((connection: MockConnection) => {
+                connection.mockRespond(new Response(returnXMLdoc(null)));
+            });
+
+            sut.getXML().subscribe((data) => {
+                // toTo: put test here...
+                assertionCall();
+            });
+
+            assertThat(assertionCall, wasCalled());
+        }));
+
+    });
 });
